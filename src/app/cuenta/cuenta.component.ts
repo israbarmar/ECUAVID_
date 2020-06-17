c4da909c390c3c093325a1cbbff60bfc9bea3a6f
israbarmar <barbotoi@yahoo.com>
2020-06-17 15:47:07 -0500

    Commit message

--- src/app/cuenta/cuenta.component.ts
+++ src/app/cuenta/cuenta.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-cuenta',
   templateUrl: './cuenta.component.html',
-  styles: [
-  ]
+  styleUrls: ['./cuenta.component.css']
 })
-export class CuentaComponent implements OnInit {
+
+export class CuentaComponent implements OnInit, OnDestroy {
+  // Search the tags in the DOM
+  bodyTag: HTMLBodyElement = document.getElementsByTagName('body')[0];
+  htmlTag: HTMLElement = document.getElementsByTagName('html')[0];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    // add the css-style to the html and body tags
+    this.bodyTag.classList.add('cuenta-page');
+    this.htmlTag.classList.add('cuenta-page');
   }
 
-}
+  ngOnDestroy() {
+    // remove the the body classes
+    this.bodyTag.classList.remove('cuenta-page');
+    this.htmlTag.classList.remove('cuenta-page');
+  }
+
+  }
