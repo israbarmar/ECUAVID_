535523a602700965473170e589f5aa0bb420e2cf
israbarmar <barbotoi@yahoo.com>
2020-06-18 17:39:25 -0500

    Commit message

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,11 +10,11 @@ import { Color, Label } from 'ng2-charts';
 export class HomeComponent implements OnInit {
   
   lineChartData: ChartDataSets[] = [
-    { data: [1700, 1500, 5100, 4700, 5500, 8000, 5000,4750,3000,2750], label: 'Contagiados' },
-    { data: [270, 320, 1250, 760, 1100, 2678, 1000,657,300,275], label: 'Fallecidos' }
+    { data: [1700, 1500, 5100, 4700, 5500], label: 'Contagiados' },
+    { data: [270, 320, 1250, 760, 1100], label: 'Fallecidos' }
   ];
     //Labels shown on the x-axis
-    lineChartLabels: Label[] = ['Marzo', 'Abril', 'Mayo', 'Junio', 'Julio','Agosto', 'Septiembre','Octubre','Noviembre','Diciembre'];
+    lineChartLabels: Label[] = ['Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
 
     // Define chart options
     lineChartOptions: ChartOptions = {
@@ -43,13 +43,56 @@ export class HomeComponent implements OnInit {
     lineChartPlugins = [];
   
     // events
-    chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    chartClickedD({ event, active }: { event: MouseEvent, active: {}[] }): void {
       console.log(event, active);
     }
   
-    chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    chartHoveredD({ event, active }: { event: MouseEvent, active: {}[] }): void {
       console.log(event, active);
     }
+
+    lineChartDataD: ChartDataSets[] = [
+      { data: [400, 450, 500, 550, 570, 575, 600], label: 'Contagiados' },
+      { data: [300, 350, 320, 400, 455, 370, 400], label: 'Fallecidos' }
+    ];
+      //Labels shown on the x-axis
+      lineChartLabelsD: Label[] = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes','Sábado', 'Domingo'];
+  
+      // Define chart options
+      lineChartOptionsD: ChartOptions = {
+        responsive: true
+      };
+    
+      // Define colors of chart segments
+      lineChartColorsD: Color[] = [
+    
+        { // dark grey
+          backgroundColor: 'rgba(77,83,96,0.2)',
+          borderColor: 'blue',
+        },
+        { // red
+          backgroundColor: 'rgba(255,0,0,0.3)',
+          borderColor: 'red',
+        }
+      ];
+    
+      // Set true to show legends
+      lineChartLegendD = true;
+    
+      // Define type of chart
+      lineChartTypeD = 'line';
+    
+      lineChartPluginsD = [];
+    
+      // events
+      chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+        console.log(event, active);
+      }
+    
+      chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+        console.log(event, active);
+      }
+    
   
   constructor() { }
 
