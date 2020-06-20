8995056056aead04d29acdb9afe36903077ddb18
israbarmar <barbotoi@yahoo.com>
2020-06-20 13:30:48 -0500

    Commit message

--- src/app/etnia/etnia.component.ts
+++ src/app/etnia/etnia.component.ts
@@ -9,17 +9,40 @@ import { Color, Label } from 'ng2-charts';
 })
 export class EtniaComponent implements OnInit {
 
-    lineChartDataE: ChartDataSets[] = [
-      { data: [200, 400, 600, 550, 380,513,598], label: 'Contagiados' },
-      { data: [50, 80, 193, 260, 200, 250, 157], label: 'Fallecidos' }
+    lineChartDataEM: ChartDataSets[] = [
+      { data: [100, 170, 300, 250, 180, 213, 198], label: 'Contagiados' },
+      { data: [50, 80, 93, 26, 20, 25, 15], label: 'Fallecidos' }
     ];
 
-    lineChartDataEM: ChartDataSets[] = [
-      { data: [200, 400, 600, 550, 380,513,598], label: 'Contagiados' },
-      { data: [50, 80, 193, 260, 200, 250, 157], label: 'Fallecidos' }
+    lineChartDataEMT: ChartDataSets[] = [
+      { data: [500, 450, 600, 550, 380, 513, 598], label: 'Contagiados' },
+      { data: [150, 180, 293, 360, 200, 250, 157], label: 'Fallecidos' }
     ];
+
+    lineChartDataEI: ChartDataSets[] = [
+      { data: [120, 275, 400, 440, 380, 487, 200], label: 'Contagiados' },
+      { data: [43, 20, 33, 160, 120, 150, 150], label: 'Fallecidos' }
+    ];
+
+    lineChartDataEA: ChartDataSets[] = [
+      { data: [20, 75, 40, 44, 38, 48, 20], label: 'Contagiados' },
+      { data: [3, 12, 23, 16, 12, 15, 22], label: 'Fallecidos' }
+    ]; 
+
+    lineChartDataEAf: ChartDataSets[] = [
+      { data: [45, 70, 48, 24, 28, 58, 21], label: 'Contagiados' },
+      { data: [13, 10, 3, 6, 19, 35, 18], label: 'Fallecidos' }
+    ]; 
+
+    lineChartDataEB: ChartDataSets[] = [
+      { data: [25, 78, 58, 14, 38, 50, 41], label: 'Contagiados' },
+      { data: [2, 19, 32, 4, 11, 22, 20], label: 'Fallecidos' }
+    ]; 
+
       //Labels shown on the x-axis
       lineChartLabelsE: Label[] = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes','Sábado', 'Domingo'];
+
+      lineChartLabelsET: Label[] = ['Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
   
       // Define chart options
       lineChartOptionsE: ChartOptions = {
@@ -27,29 +50,25 @@ export class EtniaComponent implements OnInit {
       };
     
       // Define colors of chart segments
-      lineChartColorsE: Color[] = [
-    
+      lineChartColorsEH: Color[] = [
+
         { // dark grey
           backgroundColor: 'rgba(77,83,96,0.2)',
           borderColor: 'green',
         },
-        { // red
+        { // amarillo
           backgroundColor: 'rgba(244, 247, 118, 0.5)',
           borderColor: 'yellow',
         }
       ];
 
-      lineChartColorsEM: Color[] = [
-    
-        { // dark grey
-          backgroundColor: 'rgba(77,83,96,0.2)',
-          borderColor: 'green',
-        },
-        { // red
-          backgroundColor: 'rgba(244, 247, 118, 0.5)',
-          borderColor: 'yellow',
-        }
-      ];
+      lineChartColorsET: Color[] = [
+      { // dark grey
+        backgroundColor: 'rgba(255,69,0,0)',
+        borderColor: 'orange',
+      },
+    ];
+      
     
       // Set true to show legends
       lineChartLegendE = true;
